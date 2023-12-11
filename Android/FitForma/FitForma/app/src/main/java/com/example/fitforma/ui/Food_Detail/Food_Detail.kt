a3712f1ca7df09d99129aba8caf10cf8a4b53fe8
Xnite-X <48861223+Xnite-X@users.noreply.github.com>
2023-12-11 14:22:49 +0700

    Bikin Food Activity dan Scan Activity

---
+++ Android/FitForma/FitForma/app/src/main/java/com/example/fitforma/ui/Food_Detail/Food_Detail.kt
@@ -0,0 +1,12 @@
+package com.example.fitforma.ui.Food_Detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitforma.R
+
+class Food_Detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_detail)
+    }
+}
--- Android/FitForma/FitForma/app/src/main/java/com/example/fitforma/ui/Scan/ScanActivity.kt
+++ Android/FitForma/FitForma/app/src/main/java/com/example/fitforma/ui/Scan/ScanActivity.kt
@@ -1,12 +1,12 @@
-package com.example.fitforma.ui
+package com.example.fitforma.ui.Scan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fitforma.R
 
-class Food_Detail : AppCompatActivity() {
+class ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food_detail)
+        setContentView(R.layout.activity_scan)
     }
 }
