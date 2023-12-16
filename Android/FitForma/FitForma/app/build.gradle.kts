b8b3ff9e6c512c86a1a64efe7033256927742f30
Nathan Ariel Phedra <nathan.ariel@ti.ukdw.ac.id>
2023-12-17 02:49:02 +0700

    revisi search

--- Android/FitForma/FitForma/app/build.gradle.kts
+++ Android/FitForma/FitForma/app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation ("com.google.firebase:firebase-auth:22.3.0")
     implementation ("com.google.android.material:material:1.10.0")
     implementation("androidx.exifinterface:exifinterface:1.3.6")
+    implementation("com.google.firebase:firebase-firestore:24.10.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
