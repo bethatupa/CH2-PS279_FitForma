e7d5a0b7e8f8d0c079f70a8ed6f40ff0003d4305
Nathan Ariel Phedra <nathan.ariel@ti.ukdw.ac.id>
2023-12-19 15:44:40 +0700

    DONE retrieve data on account

--- Android/FitForma/FitForma/app/src/main/java/com/example/fitforma/ui/account/AccountFragment.kt
+++ Android/FitForma/FitForma/app/src/main/java/com/example/fitforma/ui/account/AccountFragment.kt
@@ -2,24 +2,26 @@ package com.example.fitforma.ui.account
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.fitforma.databinding.FragmentAccountBinding
 import com.example.fitforma.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AccountFragment : Fragment() {
     private var _binding: FragmentAccountBinding? = null
-    lateinit var firebaseAuth: FirebaseAuth
-    private val binding get() = _binding !!
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val db = FirebaseFirestore.getInstance()
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -34,18 +36,57 @@ class AccountFragment : Fragment() {
         firebaseAuth = FirebaseAuth.getInstance()
         val user = firebaseAuth.currentUser
 
-        if (user != null){
-            binding.usernameTextView.text = user.displayName  //blm nyambung ke firestore
-            binding.emailTextView.text = user.email
+        if (user != null) {
+            // Get the user document reference based on the user's UID
+            val userRef = db.collection("users").document(user.uid)
+
+            // Retrieve the user's data
+            userRef.get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val document: DocumentSnapshot? = task.result
+                    if (document != null && document.exists()) {
+                        val name = document.getString("name")
+                        val email = document.getString("email")
+                        val age = document.getString("age")
+                        val gender = document.getString("gender")
+                        val weight = document.getString("weight")
+                        val height = document.getString("height")
+
+                        binding.usernameTextView.text = name
+                        binding.emailTextView.text = email
+
+                        if (!age.isNullOrEmpty()) {
+                            binding.ageTextView.visibility = View.VISIBLE
+                            binding.ageTextView.text = "Age: $age"
+                        }
+
+                        if (!gender.isNullOrEmpty()) {
+                            binding.genderTextView.visibility = View.VISIBLE
+                            binding.genderTextView.text = "Age: $gender"
+                        }
+
+                        if (!weight.isNullOrEmpty()) {
+                            binding.weightTextView.visibility = View.VISIBLE
+                            binding.weightTextView.text = "Weight: $weight"
+                        }
+
+                        if (!height.isNullOrEmpty()) {
+                            binding.heightTextView.visibility = View.VISIBLE
+                            binding.heightTextView.text = "Height: $height"
+                        }
+                    }
+                } else {
+                    // Handle errors
+                }
+            }
         }
 
-        binding.logoutBtn.setOnClickListener{
+        binding.logoutBtn.setOnClickListener {
             logout()
         }
     }
 
     private fun logout() {
-        firebaseAuth = FirebaseAuth.getInstance()
         firebaseAuth.signOut()
         val i = Intent(context, LoginActivity::class.java)
         startActivity(i)
